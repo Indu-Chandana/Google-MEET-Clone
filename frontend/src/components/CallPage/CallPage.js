45373a24c82117e707e84228c07ea991f2346fc9
I.Chandana <72302495+Indu-Chandana@users.noreply.github.com>
2021-09-17 19:06:17 +0530

    Add files via upload

---
+++ frontend/src/components/CallPage/CallPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CallPage.scss'
+
+function CallPage() {
+    return (
+        <div>
+            <h1>CallPage</h1>
+        </div>
+    )
+}
+
+export default CallPage
